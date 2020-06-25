57cc5a8060668c1ebc00735581a0202a45c47277
ddduuufff <ericduford@gmail.com>
2020-06-25 12:30:06 -0400

    changed package file to load libraries

--- packages/eric_packages.R
+++ packages/eric_packages.R
@@ -1,7 +1,15 @@
+eric_apps <- function(){
+#packages
 eric_packages <- c(
   "dplyr",
   "tidyr",
   "stringr",
   "lubridate",
   "ggplot2"
-)
+)#We create a variable where we indicating that we select the ones that we do not have installed.                     
+install.lib <- eric_packages[!eric_packages %in% installed.packages()]
+#With a loop we install the libraries that we do not have.
+for(lib in install.lib) install.packages(lib,dependences=TRUE)
+#Load the libraries
+sapply(eric_packages,require,character=TRUE)
+}
